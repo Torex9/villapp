d7078e85582d88b71d6a81b41a9a81ba5285c4ff
Rufus Oludare <toludare68@gmail.com>
2025-08-09 21:00:31 +0100

    changed cookies bacground colour

--- components/02-molecules/CookieConsentPopup.tsx
+++ components/02-molecules/CookieConsentPopup.tsx
@@ -1,18 +1,20 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import CookieConsent from 'react-cookie-consent';
+import Link from "next/link";
+import CookieConsent from "react-cookie-consent";
 
 export interface CookieConsentPopupProps {
   privacyPolicyLink: string;
 }
 
-export default function CookieConsentPopup({ privacyPolicyLink }: CookieConsentPopupProps) {
+export default function CookieConsentPopup({
+  privacyPolicyLink,
+}: CookieConsentPopupProps) {
   return (
     <CookieConsent
       disableStyles={true}
-      containerClasses="fixed bottom-0 right-0 p-6 bg-alpha-dark-900 text-white flex flex-wrap items-center justify-between gap-6 md:m-6 md:rounded-md md:shadow-md z-50"
-      buttonClasses="border border-white px-4 py-2 rounded"
+      containerClasses="fixed bottom-0 right-0 p-6 bg-[#ed5a2e] text-white flex flex-wrap items-center justify-between gap-6 md:m-6 md:rounded-md md:shadow-md z-50"
+      buttonClasses="border border-white px-4 py-2 rounded hover:bg-white hover:text-[#ed5a2e] transition-colors duration-200"
     >
       <p>We use cookies to get a better understanding of our visitors</p>
       <Link href={privacyPolicyLink} className="text-sm underline">
